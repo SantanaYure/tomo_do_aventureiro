148666e35e3f50da6ee6725b0ffb0a328eb4cc08
santanaYure <yure.s.santana@outlook.com>
2025-10-04 11:29:39 -0300

    config: Preparando o projeto para iniciar as implementções

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { CharacterCreationComponent } from './components/character-creation/character-creation.component';
-import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  {
-    path: 'character-creation',
-    component: CharacterCreationComponent,
-    canActivate: [AuthGuard],
-  },
   { path: '**', redirectTo: '/login' }, // Wildcard route para páginas não encontradas
 ];
